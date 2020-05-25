205ebe41f0ac02e68b258728855f59e4173e2f0e
wn0824 <wn0824@gmail.com>
2020-05-25 15:23:37 +0900

    Plus title text and clock js.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 hello! this is my sketchbook.
 
 2020-05-25
-    Plus to toggle-menu! this is amazing.
+ Plus to toggle-menu! this is amazing.
+ Plus Title Text, Clock js.
+
--- js/mainPage.js
+++ js/mainPage.js
@@ -1,5 +1,17 @@
+const CLOCK = document.querySelector(".mainClock"),
+      WRITE_CLOCK = CLOCK.querySelector("span");
 
-
-function firstMenubar(){
-    
+function mainClock(){
+    const date = new Date;
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+    WRITE_CLOCK.innerText = `${hour < 10 ? `0${hour}` : hour}:${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}`;
 }
+
+function init(){
+    mainClock();
+    setInterval(mainClock, 1000);
+}
+
+init();
