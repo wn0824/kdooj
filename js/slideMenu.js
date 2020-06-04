3c5920e2478bb998440b01fe94366ac6d8235474
wn0824 <wn0824@gmail.com>
2020-06-04 10:31:44 +0900

    add slide menu. and delete some content.

---
+++ js/slideMenu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.querySelector('.slideMenu').style.width = "250px";
+}
+
+function closeMenu() {
+    document.querySelector('.slideMenu').style.width = "0px";
+}
+
+// this menubar script reference for https://www.w3schools.com/howto/howto_js_sidenav.asp
